595fc2b399308dd0797fc93725d30ad43b1eeed0
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-21 21:50:11 +0800

    ext matintain

--- app/models/crm/maintain.rb
+++ app/models/crm/maintain.rb
@@ -2,6 +2,7 @@ module Crm
   class Maintain < ApplicationRecord
     include Model::Maintain
     include Factory::Ext::Maintain if defined? RailsFactory
+    include Wechat::Ext::Maintain if defined? RailsWechat
     include Eventual::Ext::Planned if defined? RailsEvent
   end
 end
