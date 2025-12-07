4d29e660133645fde308ea0548582587a5dd32f1
qinmingyuan <mingyuan0715@foxmail.com>
2025-12-07 21:24:26 +0800

    fix

--- app/models/crm/ext/maintainable.rb
+++ app/models/crm/ext/maintainable.rb
@@ -3,12 +3,15 @@ module Crm
     extend ActiveSupport::Concern
 
     included do
-      #belongs_to :maintain, class_name: 'Crm::Maintain', counter_cache: true, optional: true
+      belongs_to :maintain, class_name: 'Crm::Maintain', optional: true, foreign_key: [:contact_id, :agent_id], primary_key: [:contact_id, :agent_id]
+
       belongs_to :client, class_name: 'Crm::Client', optional: true
-      belongs_to :contact, class_name: 'Crm::Contact', optional: true
-      belongs_to :agent, class_name: 'Org::Member', optional: true
       accepts_nested_attributes_for :client
+
+      belongs_to :contact, class_name: 'Crm::Contact', optional: true
       accepts_nested_attributes_for :contact
+
+      belongs_to :agent, class_name: 'Org::Member', optional: true
 
       #before_save :sync_from_maintain, if: -> { client_id.present? && maintain_id_changed? }
       before_validation :sync_from_contact, if: -> { (changes.keys & ['contact_id']).present? }
