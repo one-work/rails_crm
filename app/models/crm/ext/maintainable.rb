d80c4cc5c9d1e3a7039e0420da6b17611982de24
qinmingyuan <mingyuan0715@foxmail.com>
2025-12-07 22:45:23 +0800

    contact

--- app/models/crm/ext/maintainable.rb
+++ app/models/crm/ext/maintainable.rb
@@ -11,11 +11,14 @@ module Crm
       belongs_to :contact, class_name: 'Crm::Contact', optional: true
       accepts_nested_attributes_for :contact
 
+      has_one :client_contact, class_name: 'Crm::Contact', primary_key: [:user_id, :organ_id], foreign_key: [:client_user_id, :organ_id]
+
       belongs_to :agent, class_name: 'Org::Member', optional: true
 
       #before_save :sync_from_maintain, if: -> { client_id.present? && maintain_id_changed? }
-      before_validation :sync_from_contact, if: -> { (changes.keys & ['contact_id']).present? }
+      before_validation :sync_from_contact, if: -> { (changes.keys & ['contact_id']).present? || user_id.present? }
       before_validation :sync_from_client, if: -> { (changes.keys & ['client_id']).present? }
+      before_validation :sync_with_client_contact, if: -> { contact_id.blank? && user_id.present? }
 
       #after_create :change_maintain_state, if: -> { maintain_id.present? && saved_change_to_maintain_id? }
     end
@@ -35,7 +38,12 @@ module Crm
 
     def sync_from_contact
       return unless contact
-      self.assign_attributes contact.xx_options
+      self.assign_attributes contact.client_options
+    end
+
+    def sync_with_client_contact
+      client_contact || build_client_contact
+      self.contact = client_contact
     end
 
     def sync_from_client
--- app/models/crm/model/contact.rb
+++ app/models/crm/model/contact.rb
@@ -122,7 +122,7 @@ module Crm
       client_user.wallets.where(organ_id: organ_id, contact_id: nil).update_all contact_id: id
     end
 
-    def xx_options
+    def client_options
       {
         user_id: client_user_id,
         member_id: client_member_id,
