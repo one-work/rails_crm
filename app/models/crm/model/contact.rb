f71d1d7d99f81e0686223d71f47e27ecbc1a0bcb
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-28 17:47:08 +0800

    sync form client user

--- app/models/crm/model/contact.rb
+++ app/models/crm/model/contact.rb
@@ -98,6 +98,7 @@ module Crm
     def sync_from_client_user
       self.name = client_user.name if self.name.blank?
       self.avatar_blob ||= client_user.avatar_blob
+      self.identity ||= client_user.oauth_users.pluck(:identity)[0]
     end
 
     def init_member_organ!
