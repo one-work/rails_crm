a7b2f7440968fcea723e648406217af584354885
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-24 15:20:19 +0800

    fixed

--- app/controllers/crm/admin/productions_controller.rb
+++ app/controllers/crm/admin/productions_controller.rb
@@ -8,7 +8,7 @@ module Crm
       q_params = {}
       q_params.merge! default_params
       q_params.merge! production_plans: { produce_on: params[:produce_on], scene_id: params[:scene_id] } if params[:produce_on] && params[:scene_id]
-      q_params.merge! params.permit(:organ_id, :factory_taxon_id, 'name-like')
+      q_params.merge! params.permit(:organ_id, :taxon_id, 'name-like')
 
       @productions = Factory::Production.includes(:production_plans, :parts, product: [:brand, { logo_attachment: :blob }]).default_where(q_params).default.order(id: :desc).page(params[:page]).per(params[:per])
     end
